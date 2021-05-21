e042a5d2db7b012d2e3291db1044d22b6e79c375
Saravana Pandian <83231084+saravana-pandian@users.noreply.github.com>
2021-05-21 15:47:55 +0530

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,1 +1,11 @@
+{
+    'name': 'Theme 2000',
+    'description': 'My New Theme',
+    'version': '1.0',
+    'author': 'Saravana',
+    'category': 'Theme/Creative',
 
+    'depends': ['website'],
+    'data': [
+    ],
+}
